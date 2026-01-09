36796daa1f66bc6d51c66b1451a6190bae2a8a51
Ramprasad <ramprasad@kissflow.com>
2026-01-09 11:28:50 +0530

    refactor: remove unused export of BackendSchema from useForm hook index

--- app/sources/index.ts
+++ app/sources/index.ts
@@ -1,6 +1,5 @@
 // App-specific sources (dynamic, user-configurable)
 export * from "./product";
 export * from "./order";
-export * from "./ecommerce/product";
 export * from "./ecommerce/cart";
 export * from "./ecommerce/restocking";
